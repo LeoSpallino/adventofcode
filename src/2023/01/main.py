a356c0712df973ea361ac8f300d9a13ace622c55
Leo Spallino <spallino@icloud.com>
2023-12-01 23:56:56 -0500

    Use sliding window to find digits in one pass in place

--- src/2023/01/main.py
+++ src/2023/01/main.py
@@ -1,38 +1,80 @@
 def stringToDigits(line):
-    digits = []
-    for i in line:
-        if ord(i) in range(ord("0"), ord("9") + 1):
-            digits.append(i)
+    leftDigit = ""
+    rightDigit = ""
+    reversedLine = line[::-1]
 
-    return digits
+    for i in range(len(line)):
+        if leftDigit == "" and ord(line[i]) in range(ord("0"), ord("9") + 1):
+            leftDigit = line[i]
+
+        if rightDigit == "" and ord(reversedLine[i]) in range(ord("0"), ord("9") + 1):
+            rightDigit = reversedLine[i]
+
+        if leftDigit != "" and rightDigit != "":
+            break
+
+    return int(leftDigit + rightDigit)
 
 
-def wordToDigits(line):
+def wordAndNumsToDigits(line):
     wordLookup = {
-        "one": "o1e",
-        "two": "t2o",
-        "three": "t3e",
-        "four": "f4r",
-        "five": "f5e",
-        "six": "s6x",
-        "seven": "s7n",
-        "eight": "e8t",
-        "nine": "n9e",
+        "one": "1",
+        "two": "2",
+        "three": "3",
+        "four": "4",
+        "five": "5",
+        "six": "6",
+        "seven": "7",
+        "eight": "8",
+        "nine": "9",
     }
-    for i in range(len(line) - 4):
-        for key in wordLookup:
-            if key in line[i : i + 5]:
-                line = line.replace(key, wordLookup[key], 1)
 
-    return line
+    reversedLine = line[::-1]
+    leftDigit = ""
+    rightDigit = ""
+    leftPointer = 0
+    rightPointer = 0
+
+    while leftDigit == "" or rightDigit == "" or leftPointer < len(line):
+        if leftDigit == "" and ord(line[leftPointer]) in range(ord("0"), ord("9") + 1):
+            leftDigit = line[leftPointer]
+        if rightDigit == "" and ord(reversedLine[leftPointer]) in range(
+            ord("0"), ord("9") + 1
+        ):
+            rightDigit = reversedLine[leftPointer]
+
+        while (
+            leftDigit == ""
+            and rightPointer < len(line)
+            and rightPointer < leftPointer + 6
+        ):
+            if line[leftPointer:rightPointer] in wordLookup:
+                leftDigit = wordLookup[line[leftPointer:rightPointer]]
+                break
+            rightPointer += 1
+
+        rightPointer = leftPointer
+
+        while (
+            rightDigit == ""
+            and rightPointer < len(reversedLine)
+            and rightPointer < leftPointer + 6
+        ):
+            if reversedLine[leftPointer:rightPointer][::-1] in wordLookup:
+                rightDigit = wordLookup[reversedLine[leftPointer:rightPointer][::-1]]
+                break
+            rightPointer += 1
+
+        leftPointer += 1
+        rightPointer = leftPointer
+
+    return int(leftDigit + rightDigit)
 
 
 def part1(data):
     total = 0
     for line in data:
-        digits = stringToDigits(line)
-        parsedNum = digits[0] + digits[-1]
-        total += int(parsedNum)
+        total += stringToDigits(line)
 
     return total
 
@@ -40,10 +82,7 @@ def part1(data):
 def part2(data):
     total = 0
     for line in data:
-        parsedWords = wordToDigits(line)
-        digits = stringToDigits(parsedWords)
-        parsedNum = digits[0] + digits[-1]
-        total += int(parsedNum)
+        total += wordAndNumsToDigits(line)
     return total
 
 
