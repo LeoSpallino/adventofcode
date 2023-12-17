0152737eb3d37a8db93a403c5b117e24bb08c499
Leo Spallino <spallino@icloud.com>
2023-12-16 23:01:10 -0500

    Use complex nums to represent points for day 16

--- src/2023/16/main.py
+++ src/2023/16/main.py
@@ -1,12 +1,4 @@
-from enum import Enum
-
 grid = open("testcase.in").read().splitlines()
-
-
-def isOutOfBounds(coord: tuple[int, int]):
-    row, col = coord
-    return row < 0 or col < 0 or row >= len(grid) or col >= len(grid[0])
-
 
 UP, DOWN, RIGHT, LEFT = (0 - 1j), (0 + 1j), (1 + 0j), (-1 + 0j)
 tileLookup = {
@@ -18,46 +10,20 @@ tileLookup = {
 }
 
 
-def getNextBeamPos(coord: tuple[int, int], dir: str):
-    row, col = coord
-    if grid[row][col] in ".|-":
-        if grid[row][col] == "|" and (dir == "RIGHT" or dir == "LEFT"):
-            return [((row + 1, col), "DOWN"), ((row - 1, col), "UP")]
-        if grid[row][col] == "-" and (dir == "UP" or dir == "DOWN"):
-            return [((row, col - 1), "LEFT"), ((row, col + 1), "RIGHT")]
-        else:
-            if dir == "LEFT":
-                return [((row, col - 1), "LEFT")]
-            if dir == "RIGHT":
-                return [((row, col + 1), "RIGHT")]
-            if dir == "UP":
-                return [((row - 1, col), "UP")]
-            if dir == "DOWN":
-                return [((row + 1, col), "DOWN")]
-    if grid[row][col] == "/":
-        if dir == "LEFT":
-            return [((row + 1, col), "DOWN")]
-        elif dir == "RIGHT":
-            return [((row - 1, col), "UP")]
-        elif dir == "UP":
-            return [((row, col + 1), "RIGHT")]
-        elif dir == "DOWN":
-            return [((row, col - 1), "LEFT")]
-    if grid[row][col] == "\\":
-        if dir == "LEFT":
-            return [((row - 1, col), "UP")]
-        elif dir == "RIGHT":
-            return [((row + 1, col), "DOWN")]
-        elif dir == "UP":
-            return [((row, col - 1), "LEFT")]
-        elif dir == "DOWN":
-            return [((row, col + 1), "RIGHT")]
-    return []
+def isOutOfBounds(coord: complex):
+    row, col = int(coord.imag), int(coord.real)
+    return row < 0 or col < 0 or row >= len(grid) or col >= len(grid[0])
 
 
-def traverse(startPoint, startDir):
-    visited: dict[tuple[int, int], set[str]] = {}
-    beams = [(startPoint, startDir)]
+def getNextBeamPos(coord: complex, dir: complex):
+    row, col = int(coord.imag), int(coord.real)
+    next = tileLookup[grid[row][col]][dir]
+    return [(coord + n, n) for n in next]
+
+
+def traverse(startPoint: tuple[int, int], startDir: complex):
+    visited: dict[complex, set[complex]] = {}
+    beams = [(complex(*startPoint), startDir)]
 
     while len(beams):
         coord, dir = beams.pop()
@@ -79,14 +45,14 @@ highestEnergy = 0
 for i in range(len(grid)):
     highestEnergy = max(
         highestEnergy,
-        traverse((i, 0), "RIGHT"),
-        traverse((i, len(grid[0]) - 1), "LEFT"),
+        traverse((0, i), RIGHT),
+        traverse((len(grid[0]) - 1, i), LEFT),
     )
 
 for i in range(len(grid[0])):
     highestEnergy = max(
-        highestEnergy, traverse((0, i), "DOWN"), traverse((len(grid) - 1, i), "UP")
+        highestEnergy, traverse((i, 0), DOWN), traverse((i, len(grid) - 1), UP)
     )
 
-print("Part 1:", traverse((0, 0), "RIGHT"))
+print("Part 1:", traverse((0, 0), RIGHT))
 print("Part 2:", highestEnergy)
